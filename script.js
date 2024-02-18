5416a00289393e8e68476719bf24421f495cd678
Nicholas <nsheppard999@gmail.com>
2024-02-18 14:24:41 -0400

    Create win condition
    The game is now winnable with the checkWinCondition function,
    which checks if any possible win condition has been met
    after a turn.
    
    Next steps are to clean the code and create a UI

--- script.js
+++ script.js
@@ -38,8 +38,9 @@ function Gameboard() {
 
     //Prints the board to the console after each players turn
     const printBoard = () => {
-        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
-        console.log(boardWithCellValues);
+        const boardwithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardwithCellValues);
+        return boardwithCellValues;
     }
 
     //Provide interactable interface for application
@@ -92,6 +93,7 @@ function GameController(playerOneName = "Nicholas", playerTwoName = "Guest") {
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     };
+
     //Will be used for console declaration and later for UI 
     const getActivePlayer = () => activePlayer;
 
@@ -100,23 +102,58 @@ function GameController(playerOneName = "Nicholas", playerTwoName = "Guest") {
         console.log(`${getActivePlayer().name}'s turn.`)
     }
 
+    const checkWinCondition = () => {
+        const currentBoard = board.printBoard();
+        //Check for horizontal win conditions
+        for (let row = 0; row < 3; row++) {
+            if (currentBoard[row][0] === currentBoard[row][1] && currentBoard[row][1] === currentBoard[row][2] && currentBoard[row][0] !== 0) {
+                return true;
+            }
+        }
+
+        //Check for vertical win conditions
+        for (let col = 0; col < 3; col++) {
+            if (currentBoard[0][col] === currentBoard[1][col] && currentBoard[1][col] === currentBoard[2][col] && currentBoard[0][col] !== 0) {
+                return true;
+            }
+        }
+
+        //Check for diagonal win conditions
+        if ((currentBoard[0][0] === currentBoard[1][1] && currentBoard[1][1] === currentBoard[2][2] && currentBoard[0][0] !== 0) ||
+            (currentBoard[0][2] === currentBoard[1][1] && currentBoard[1][1] === currentBoard[2][0] && currentBoard[0][2] !== 0)) {
+            return true;
+        }
+
+        return false;
+    }
+
     const playRound = (column, row) => {
 
         //Place player mark on desired column and row
         console.log(`Marking ${column},${row} with ${getActivePlayer().name}'s mark: ${getActivePlayer().mark}`);
         let validMove = board.placeMarker(column, row, getActivePlayer().mark)
+        
+
+        //Check whether a player has won, break if they have
+        let playerWon = checkWinCondition();
+        console.log(playerWon)
+
+        if (playerWon === true) {
+            console.log(`CONGRATULATIONS ${getActivePlayer().name}!! YOU WON!!!`);
+            return;
+        }
 
         //Check whether the desired column and row are available, do not switch turns
         //on an invalid move
         if (validMove === true){
-
             //Switch turn
-            switchPlayerTurn();
+            switchPlayerTurn();    
         }
 
         printNewRound();
+
     }
-    
+
 
     //Initial boot display
     printNewRound();
